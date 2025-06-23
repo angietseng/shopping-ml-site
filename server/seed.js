51f556e3fdbef8e99e47d8cc3d2218536c8a5848
Angeline Tseng <angelinecct3@gmail.com>
2025-06-23 17:27:39 -0400

    Add more items to check out the page layout

--- server/seed.js
+++ server/seed.js
@@ -20,6 +20,27 @@ mongoose.connect(process.env.MONGO_URI).then(async () => {
       imageUrl: "https://media.sezane.com/image/upload/c_fill,d_placeholder_dark.png,fl_progressive:semi,h_816,q_auto:best,w_582/kjyxzs8lcgas6fbmywuv.jpg",
       tags: ["denim", "jeans"],
     },
+    {
+      name: "Black Belt",
+      description: "Italian leather",
+      price: 15,
+      imageUrl: "https://media.sezane.com/image/upload/c_fill,d_placeholder_dark.png,fl_progressive:semi,h_816,q_auto:best,w_582/g7jprovxddwltxaaeqdn.jpg",
+      tags: ["belt", "leather"],
+    },
+    {
+      name: "Red Ballerinas",
+      description: "Sacchetto style",
+      price: 70,
+      imageUrl: "https://media.sezane.com/image/upload/c_fill,d_placeholder_dark.png,fl_progressive:semi,h_816,q_auto:best,w_582/jrg1g12gvlfca3qm9f6r.jpg",
+      tags: ["ballerinas", "red"],
+    },
+    {
+      name: "Bamboo Basket Bag",
+      description: "Carried on the Shoulder",
+      price: 110,
+      imageUrl: "https://media.sezane.com/image/upload/c_fill,d_placeholder_dark.png,fl_progressive:semi,h_816,q_auto:best,w_582/alhg1kim5pqql8tqsket.jpg",
+      tags: ["bag", "shoulder bag"],
+    },
   ]);
 
   console.log("Sample products added!");
