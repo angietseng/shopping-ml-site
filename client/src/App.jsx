2d637649ca9c249fc4f29c83398bf8cc76b243f7
Angeline Tseng <angelinecct3@gmail.com>
2025-06-25 22:21:13 -0400

    Create handleAddToCart function (if product in cart, increase quantity; else add to cart)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,20 @@ import { useState } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
+  const handleAddToCart = (product) => {
+    setCart(prevCart => {
+      const existing = prevCart.find(item => item._id === product._id);
+      if (existing) {
+        return prevCart.map(item =>
+          item._id === product._id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
   return (
     <Router>
       <nav style={{ padding: "10px", backgroundColor: "#f0f0f0" }}>
