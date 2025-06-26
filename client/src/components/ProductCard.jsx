2716d9af4493c6b9bf74835bce8f768d44d3ed66
Angeline Tseng <angelinecct3@gmail.com>
2025-06-25 22:40:33 -0400

    Bug fix on ProductCard.jsx - adding export default ProductCard

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,14 @@
+import React from "react";
+
 function ProductCard({ product, onAddToCart }) {
   return (
     <div className="product-card">
       <img src={product.imageUrl} alt={product.name} className="product-image" />
       <h3>{product.name}</h3>
-      <p>${product.price}</p>
+      <p style={{ color: "black" }}>${product.price}</p>
       <button onClick={() => onAddToCart(product)}>Add to Cart</button>
     </div>
   );
 }
+
+export default ProductCard;
